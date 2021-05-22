33e184566e38eaba20c2fe8881e9c2e688cd7021
Burlutskiy-SP <burlutskiy.s.pa@sberbank.ru>
2021-05-22 20:48:59 +0600

    lesson12 copy example as is

---
+++ ATM/src/main/java/ru/sber/javaschool/examplejdbc/ExampleJDBC.java
@@ -0,0 +1,45 @@
+package ru.sber.javaschool.examplejdbc;
+
+import java.sql.*;
+import java.util.Optional;
+
+public class ExampleJDBC {
+    private String url;
+
+    public ExampleJDBC(String url) {
+        this.url = url;
+    }
+
+    public Optional<String> selectColumn() throws SQLException {
+        try(Connection connection = getConnection().orElseThrow(JDBCConnectionException::new)) {
+            try(Statement statement = connection.createStatement()) {
+                try(ResultSet resultSet = statement.executeQuery("select name from machine")) {
+                    if (resultSet.next()) {
+                        return Optional.of(resultSet.getString("NAME"));
+                    } else {
+                        return Optional.empty();
+                    }
+                }
+            }
+        }
+    }
+
+    public Optional<String> selectTwoColumns() throws SQLException {
+        try(Connection connection = getConnection().orElseThrow(JDBCConnectionException::new)) {
+            try(Statement statement = connection.createStatement()) {
+                try(ResultSet resultSet = statement.executeQuery("select name, age from animal")) {
+                    if (resultSet.next()) {
+                        return Optional.of(String.format("Name: %s, age: %d", resultSet.getString("NAME"), resultSet.getInt("age")));
+                    } else {
+                        return Optional.empty();
+                    }
+                }
+            }
+        }
+    }
+
+    private Optional<Connection> getConnection() throws SQLException {
+        return Optional.ofNullable(DriverManager.getConnection(url));
+    }
+
+}
